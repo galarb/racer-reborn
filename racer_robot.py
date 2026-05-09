6df448066635410722370ad97d7faec78739c21d
gal arbel <84223483+galarb@users.noreply.github.com>
2026-05-09 10:07:14 +0300

    Update racer_robot.py

--- racer_robot.py
+++ racer_robot.py
@@ -9,6 +9,7 @@ import neopixel
 from machine import Pin, SoftI2C
 from i2c_lcd import I2cLcd
 from time import sleep
+from smartneopixel import SmartNeoPixel
 
 encoder1_pin = 36
 encoder2_pin = 39
@@ -17,7 +18,10 @@ in2_pin = 25
 wheel_size = 85 # wheel size (in mm)
 trig_pin = 13
 echo_pin = 14
+TM_DIO = 18
+TM_CLK = 19
 leds_pin = 23
+Button_pin = 35
 NUM_LEDS = 2
 I2C_ADDR = 0x27
 I2C_NUM_ROWS = 4
@@ -25,28 +29,16 @@ I2C_NUM_COLS = 20
 
 # Create objects
 robot = ev3lego_zk(encoder1_pin, encoder2_pin, in1_pin, in2_pin, wheel_size)
-display = TM1650(18, 19)
-mode_button = ModeButton(pin_num=35, modes=4)
+display = TM1650(TM_DIO, TM_CLK)
+mode_button = ModeButton(pin_num=Button_pin, modes=4)
 pid = PID(54, 168, 4.3)
 us = Ultrasonic(trig_pin, echo_pin)
 controller = MotorController(robot, pid, us)
-np = neopixel.NeoPixel(Pin(leds_pin), NUM_LEDS)
+leds = SmartNeoPixel(Pin(leds_pin), NUM_LEDS)
 i2c = SoftI2C(sda=Pin(21), scl=Pin(22), freq=100000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
             
-def set_color(i, r, g, b):
-    np[i] = (g, r, b)  # Swap R and G
-def blink_idle():
-    set_color(0, 255, 0, 0)   # Red
-    set_color(1, 0, 255, 0)   # Green
-    np.write()
-    time.sleep(0.5)
 
-    # Swap colors
-    set_color(0, 0, 255, 0)   # Green
-    set_color(1, 255, 0, 0)   # Red
-    np.write()
-    time.sleep(0.5)
     
     
 while True:
@@ -57,12 +49,13 @@ while True:
 
     if mode == 0:
         lcd.hal_backlight_on()
+        leds.set_brightness(0.7)
+
         print('run distance measurement')
         lcd.move_to(8, 0)
         lcd.putstr("Mode 0")
-        set_color(0, 0, 0, 255)  # real BLUE    
-        #set_color(1, 0, 0, 255)  # real BLUE
-        np.write()
+        leds.set_color(0, 0, 0, 255)  # real BLUE    
+        leds.write()
         d = us.distance_cm()
         if d is None:
             print("Out of range")
@@ -77,9 +70,9 @@ while True:
         lcd.move_to(8, 0)
         lcd.putstr("Mode 1")
         print('run motor encoder')
-        set_color(0, 0, 255, 0)  # real GREEN    
+        leds.set_color(0, 0, 255, 0)  # real GREEN    
         #set_color(1, 0, 255, 0)  # real GREEN
-        np.write()
+        leds.write()
 
         controller.go_degrees(180)
         #robot.motgo(200)
@@ -94,9 +87,9 @@ while True:
         lcd.move_to(8, 0)
         lcd.putstr("Mode 2")
         print('go to 20 cm')
-        set_color(0, 80, 0, 80)     
+        leds.set_color(0, 80, 0, 80)     
         #set_color(1, 80, 0, 80)
-        np.write()
+        leds.write()
         #controller.go_us(20, 100)
         controller.go_degrees(0)
 
@@ -107,13 +100,15 @@ while True:
         
         
     elif mode == 3:
-        lcd.backlight_off()
+        lcd.hal_backlight_off()
         
         print('run_idle()')
         robot.brake() 
         robot.degrees = 0
         display.ShowStr('IdLE')
-        blink_idle()        
+        leds.set_brightness(0.05)
+
+        leds.blink_idle()        
 
 
 
